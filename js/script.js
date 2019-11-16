39036a2f2f8704722ab154b35240788472e96ec9
DeansRepos <54600923+DeanYRepos@users.noreply.github.com>
2019-11-16 12:54:55 -0600

    added new option to drop menu, selected option to display upon page load

--- js/script.js
+++ js/script.js
@@ -1,21 +1,32 @@
-
 const $other = $("#other-title");
-
+const $dropOption = $("#title");
 
 const $name = $("#name").focus(); //starts cursor in Name input upon loading
 
-
+//Job Role
 
 $other.hide(); //hides input field unless javascript is disabled
-const $dropOption = $("#title");
-$dropOption.change(function (e) {
 
-    if( $("#title option").eq(5).is(':checked') ){
+$dropOption.change(function (e) { //event handler showing/hiding input field when "Other" is selected in drop down options
 
-$other.show();        
-    }else{
+    if ($("#title option").eq(5).is(':checked')) {
+
+        $other.show();
+    } else {
 
         $other.hide();
     }
 
 })
+
+// T-Shirt
+let $color = $('#color'); 
+let $colorText = $("<option>Please select a T-shirt theme</option>");
+$colorText.attr('value','color-text');
+
+$color.prepend($colorText);
+$colorText.attr('selected','true');
+
+ 
+
+
